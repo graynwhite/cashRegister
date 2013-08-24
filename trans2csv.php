c86b950dc60dd978fa10a727b6c6bfd4d1307a90
graynwhite <cauleyfj@graynwhite.com>
2013-08-23 22:49:52 -0400

    Update readme  file and converted it to html via dingus Stated woring on extract of transaction file to cvs.

---
+++ trans2csv.php
@@ -0,0 +1,23 @@
+<?php
+require_once("connect.php");
+$sql = "SELECT *
+    INTO OUTFILE 'C:/Data/City.csv'
+     FIELDS TERMINATED BY ','
+     ENCLOSED BY '\"'
+     ESCAPED BY '\\'
+     LINES TERMINATED BY '\\n\\r'
+     FROM transaction; ";
+	 
+$result = mysql_query($sql);
+if(mysql_error()!=''){
+echo "Problem with request  " . $sql . "<br /> " . mysql_error();
+} else {
+echo "File created";
+}
+
+
+?>
+
+
+
+
