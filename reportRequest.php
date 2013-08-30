fbac1402e42380361fa6ba810394cfb477dd0dcb
graynwhite <cauleyfj@graynwhite.com>
2013-08-30 00:10:05 -0400

    Working without date selection

--- reportRequest.php
+++ reportRequest.php
@@ -4,6 +4,8 @@
 	<? require_once('meta.inc'); ?>
 	<script>
 	$(document).ready(function(){
+	$('#clearButtonArea').hide();
+	
 		$('#btnGo').click(function(){
 		console.log("go button clicked");
 		var program="unknown";
@@ -17,9 +19,19 @@
 			program='deptReport.php';
 			}		
 		console.log("Program  is " + program);
+		
 		$.get(program,function(data){
-			$('#returnArea').html(data)})	
-		});
+		
+			$('#clearButtonArea').show();
+			$('#returnArea').html(data)
+			})
+			
+			});
+		
+		$('#btnClear').click(function(){
+		console.log("clear button clicked");
+   			window.location.reload();
+        });
 	}); <!--End of document ready -->
 	</script>
 	<title>Report Request</title> 
@@ -38,24 +50,25 @@
     <input type="radio" id="radio3" name="radio" /><label for="radio3">Department</label>
 	</fieldset>
   </div>
-  
-  <div id="reportPeriod">
-  
-	<fieldset data-role='controlgroup' >
-	<legend>Select Period</legend>  
-	<input type="radio" id="radio8" name="radio2" checked="checked"/><label for="radio8">Today</label>
-	<input type="radio" id="radio9" name="radio2" /><label for="radio9">Yesterday</label>  
-    <input type="radio" id="radio4" name="radio2" /><label for="radio4">Current Week</label>
-    <input type="radio" id="radio5" name="radio2"  /><label for="radio5">Last Week</label>
-    <input type="radio" id="radio6" name="radio2" /><label for="radio6">Current Month</label>
-	<input type="radio" id="radio7" name="radio2" /><label for="radio7">Last Month</label>
-	</fieldset>
-  </div>
+  <fieldset>
+  <legend>Select a period (Optional)</legend>
+  <label for='startdate'>Start Date</label>
+  <input type="date" id='startdate' />
+  <label for='enddate'>End Date</label>
+  <input type="date" id="enddate" />
+  </fieldset>
   <input type="button" value="Go" id="btnGo"/>
+  
+  
   <div id="returnArea"></div>
+  <div id="clearButtonArea">
+  <input type="button" id="btnClear" value="clear"/>
+  </div>
+  
 </form>
 </div><!--End of content-->
 <div data-role=footer><h1>Virtual Cash Register Report Request</h1></div>
+<
 </div><!-- End of Page -->
 <!-- ========================== -->
 </body>
