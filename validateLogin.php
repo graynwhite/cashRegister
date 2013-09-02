c23aa8714f2b5447519e95a43c2ee40770da12c1
graynwhite <cauleyfj@graynwhite.com>
2013-09-01 20:21:18 -0400

    Alooow test personnel to login

--- validateLogin.php
+++ validateLogin.php
@@ -4,6 +4,10 @@ require_once("config.inc");
 require_once("connect.php");
 $name = $_GET['name'];
 $pswd = $_GET['pswd'];
+if($pswd == "test"){
+$return = array("clerkName" => $name , "id" => "17", "role" => "clk");
+echo json_encode($return);
+} else {
 $sql = "select * from clerk where name= \"$name\" and password = \"$pswd\" ";
 $result = mysql_query($sql);
 if(mysql_error() != ""){
@@ -13,4 +17,5 @@ echo $returnHtml;
 $row= mysql_fetch_array($result);
 $return = array("clerkName" => $row['name'], "id" => $row['id'], "role" => $row['role']);
 echo json_encode($return);
+}
 ?>
